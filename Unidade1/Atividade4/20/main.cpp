f5cc3374e3e72a62033bae369521328c7f182c72
Jorgevvs <jorgevinicius9@gmail.com>
2021-08-02 16:28:59 -0300

    Atvd 20 changing

--- Unidade1/Atividade4/20/main.cpp
+++ Unidade1/Atividade4/20/main.cpp
@@ -2,11 +2,24 @@
 #include <iostream>
 
 int main(void){
-   Cupom novoCupom("Mouse Corsair","RGB 10000DPI",3 , 130.00);
-
+   Cupom novoCupom("Mouse Corsair","RGB 10000DPI",-5 , -130.00);
+   
    std::cout << "\n==============================================" << std::endl;
    std::cout << "============== Loja do Jorgevvs ==============" << std::endl;
    std::cout << "==============================================" << std::endl;
+
+   std::cout << "\nO produto " << novoCupom.getId() << " com " << novoCupom.getDesc() << " no valor de R$" << novoCupom.getPreco() << " está indisponível!!" << std::endl;
+
+   std::cout << "\nModificando valores..." << std::endl;
+
+   novoCupom.setId("Teclado HyperX");
+   novoCupom.setDesc("Switch Blue");
+   novoCupom.setPreco(300);
+   novoCupom.setQuant(3);
+
+
+
+
    std::cout << "\nO produto " << novoCupom.getId() << " no valor de R$" << novoCupom.getPreco() << " está disponível!!" << std::endl;
    std::cout << "Sua compra de " << novoCupom.getQuant() << " items, terá o valor total de R$"<< novoCupom.calculaCupom() << std::endl;
 
